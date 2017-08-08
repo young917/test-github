5ed763abbf4f8944c0c1ab1252f952108cde1c3a
MinYoung <choecindy05@gmail.com>
2017-08-08 15:20:44 +0900

    minus 함수 변경 후 다시 추가

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,7 @@ public class Calculator{
     int a = 4;
     int b = 2;
     System.out.println( sum(a,b) );
+    System.out.println( minus(a,b) );
   }
   private static int sum(int a, int b){
     return a+b;
