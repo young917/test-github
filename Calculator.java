02547c10d4e590068f4c9eaaa7223d44646834d6
MinYoung <choecindy05@gmail.com>
2017-08-08 14:57:09 +0900

    sum 함수 추가

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+package com.mycompany.test;
+
+public class Calculator{
+
+  public static void main (String[] args){
+    int a = 4;
+    int b = 2;
+    System.out.println( sum(a,b) );
+  }
+  private static int sum(int a, int b){
+    return a+b;
+  }
+}
