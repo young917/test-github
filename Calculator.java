55f3a3eec4295abac682f3b6b90f0bce917fff5e
MinYoung <choecindy05@gmail.com>
2017-08-09 14:36:39 +0900

    GPU 이용하는 minus 함수 add

--- Calculator.java
+++ Calculator.java
@@ -3,15 +3,16 @@ package com.mycompany.test;
 public class Calculator{
 
   public static void main (String[] args){
-    int a = 4;
-    int b = 2;
-    System.out.println( sum(a,b) );
-    System.out.println( minus(a,b) );
+
+    System.out.println( "Hello World" );
   }
-  private static int sum(int a, int b){
-    return a+b;
-  }
-  private static int minus(int a,int b){
-    return a-b;
+  private static int minus(int a, int b, Object GPU){
+    int result = 0;
+    if( GPU == null ){
+      result =  a-b;
+    } else {
+      result = a-b;
+    }
+    return result;
   }
 }
